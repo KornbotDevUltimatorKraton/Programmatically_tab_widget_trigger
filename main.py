10dfac77111b6812c69fbb6b10afd8c8397f8607
Chanapai Chuadchum <kornbot359@hotmail.com>
2024-12-12 22:21:16 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+from flask import Flask,render_template,url_for,redirect,request,jsonify 
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+
+     return render_template("index3.html")
+
+if __name__ == "__main__":
+
+        app.run(debug=True,threaded=True,host="0.0.0.0",port=5894)
